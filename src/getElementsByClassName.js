7b7361c53c536544c7cec12e93ad21b9036651f6
Johnny <po_bo@hotmail.com>
2016-10-13 01:25:31 -0700

    completed getElementByClassName and JSONStringify

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,43 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+
+//our document.body have many elements inside, we need to create a function that will
+//travers through the document.body to access each element
+//then use .classList method to check the classList array if the input className exist
+//also using recursion and passing in the child node using .childNodes method to give us
+//a array list of child node so we can travers through each child node to check className
+var getElementsByClassName = function(className) {
+  //create a empty array variable so we can store a array of found node
+  var result = [];
+  //create a variable for document.body for easier readability
+  var docBody = document.body;
+  //since getElementByClassBane function will only accept className string input
+  //we need a helper function to help us travers through the nodes
+  //and hoist the function to top
+  function helperNodeSearch (node) {
+    //set base case
+    //travers through the node's classList by useing .classList method to convert to array list
+    //with all the className
+    _(node.classList).each(function(name) {
+      //if name in the classList equal to className
+      if(name === className) {
+      //push the entire node to the result
+      result.push(node);
+      }
+    });
+    //but if this node have no such className exist, then move to the following childNodes
+    //useing .childNodes method to gain an array list then travers through the childNodes list
+    //and pass in each rach childNodes to the recursion function to compair the names
+    _(node.childNodes).each(function(eachNode) {
+      //pass each node in to recursion function to check condition
+      helperNodeSearch(eachNode)
+    });
+  };
+//pass in  the docBody in the helper node search to begin
+helperNodeSearch(docBody)
+
+  //we will return array full of result back to the user
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,60 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //set base case if it is number, boolean, string etc.
+  if(typeof obj === 'number') {
+    return '' + obj + '';
+  }
+  if(obj === null) {
+    return "null";
+  }
+  if(typeof obj === 'boolean') {
+    return '' + obj + '';
+  }
+  if(typeof obj === 'string') {
+    return '' + '"' + obj + '"' + '';
+  }
+  //if input is an array
+  if(Array.isArray(obj)) {
+    //create a empty result array to store elements after being stringify
+    var result = [];
+  //if (obj.length === 0) {
+  //  return '['']'
+  //} else {
+    //if input no an empty array then travers through the array
+    for (var i = 0; i < obj.length; i++) {
+      //we will recurse stringify each element inside the array
+      //then push the value in to the result array
+      result.push(stringifyJSON(obj[i]));
+    }
+    //at the end we can manualy add "[" "]" to the end of the result array by "+" to concat it
+    //so it will remove the square [] and add in the "[" "]"
+    return "[" + result + "]"
+  }
+  //if input is an object
+  if(typeof obj === 'object'){
+    //we will create an empty array to store the values  and can easily be concat in to strings with commas ","
+    var result2 = [];
+    //and if object is empty by checking the length with the Object.keys().length method
+    if(Object.keys(obj).length === 0) {
+      //if length is 0 then return a curly string braces "{" "}"
+      return '{' + '}';
+    } else {
+      //otherwise if the object have length(key values) travers through the input object
+      for (var key in obj) {
+        //check if the object have key value pair of functions and undefined
+        if(key === 'functions' || typeof obj[key] === 'function' || key === 'undefined' || obj[key] === undefined) {
+          //ignore those functions and undefined and return a empty object string "{" "}"
+          return '{' + '}'
+        } else {
+        //if they are none of the above then we can stringify the key and the value
+        //then concat them with ":" to key : value pair
+        //and push it in to the result2 array as ["key : value"]
+        result2.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]))
+      }
+    }
+  }
+  //then convert results2 array in to string by adding "" then add "{" "}" to the end
+  return '{' + result2+'' + '}';
+}
 };
